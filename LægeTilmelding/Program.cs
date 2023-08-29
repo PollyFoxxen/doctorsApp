7133645a33d47d91b37f904121ff375c2efee804
PollyFoxxen <85847064+PollyFoxxen@users.noreply.github.com>
2023-08-29 13:13:24 +0200

    Add files via upload

---
+++ LægeTilmelding/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LægeTilmelding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
